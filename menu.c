7ffad3abcc3403e23978c9114a54be3b036dfcc6
MarkusLangeland <73274848+MarkusLangeland@users.noreply.github.com>
2023-11-09 20:13:42 +0100

    Add files via upload

--- menu.c
+++ menu.c
@@ -10,7 +10,9 @@
 #include "user_input.h"
 #include "oled.h"
 #include "can_controller.h"
+#include <time.h>
 
+long long int score = 0;
 
 void menu_init() {
 	volatile uint8_t* oled = (uint8_t*)0x13ff;
@@ -89,8 +91,11 @@ void menu_sub(int userInput, uint8_t* oled) {
 	switch (userInput) {
 		case 0:
 		oled_screen_clear();
+		go_to_pos(0,0);
 		oled_print_string("Playing game");
 		send_user_input_to_node2();
+		oled_screen_clear();
+		return;
 		break;
 		case 1:
 		oled_screen_clear();
@@ -133,9 +138,22 @@ void menu_sub(int userInput, uint8_t* oled) {
 
 
 void send_user_input_to_node2() {
+	time_t currentTime;
+	struct tm *timeInfo;
+	
 	volatile uint8_t* node2 = (uint8_t*)0x13ff;
 	while(true) {
 		userInput input = get_user_input(node2);
+		if (input.right_button_press) {
+			return;
+		}
+		go_to_pos(2,0);
+		char str[50];
+		snprintf(str, sizeof(str), "SCORE: %d", score);
+		oled_print_string(str);
+		score++;
+		//oled_print_string();
+		
 		message_type msg = {111, 8, {input.JoyX, input.JoyY, input.rightSlider, input.right_button_press}};
 		can_send(&msg);
 	}
